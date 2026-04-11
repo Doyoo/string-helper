6fe68c249c44bf0ae33117847616a77c4f820d02
Aaron <zhao_aaron@icloud.com>
2026-04-11 23:09:28 +0800

    修复引入jar导致的错误

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,9 +30,6 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/version_catalogs.html
 dependencies {
-    testImplementation(libs.junit)
-    testImplementation(libs.opentest4j)
-
     implementation(libs.gson)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
